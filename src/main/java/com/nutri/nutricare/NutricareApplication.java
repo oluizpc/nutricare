9b5453c8c34f5ce1e8795a3e28367331232d9cfd
Luiz Paullo <luizpc.contato@gmail.com>
2026-05-05 18:49:39 -0300

    chore: base inicial do projeto

---
+++ src/main/java/com/nutri/nutricare/NutricareApplication.java
@@ -0,0 +1,13 @@
+package com.nutri.nutricare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutricareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutri/nutricare/NutricareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutri.nutricare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutricareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
